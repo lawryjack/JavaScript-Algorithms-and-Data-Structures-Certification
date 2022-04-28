5dcc960de5671f63ea0b822458b88ec98391dd76
lawryjack <91913018+lawryjack@users.noreply.github.com>
2022-04-27 19:02:03 -0600

    Add files via upload

---
+++ Palindrome Checker/Palindrome.js
@@ -0,0 +1,40 @@
+function palindrome(str) {
+
+    var outArray = [];
+    str = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    var splitStr = str.split("");    
+    var reverseStr = str.split("").reverse();
+    //reverseStr = splitStr.reverse();  Find out why this statement was causing an issue with the string not being reversed when called
+
+    for (let i=0; i<splitStr.length; i++) {
+        if (splitStr[i] == reverseStr[i]) {
+            outArray.push(0);
+        }
+        else {
+            outArray.push(1);
+        }
+    }
+        
+    let sum = outArray.reduce(function(a,b) {
+        return a+b;
+    }, 0);
+       
+    if (sum != 0) {
+        return false;
+    }
+    else {
+        return true;
+    }
+    }
+    
+console.log(palindrome("eye")); //Enter any string in place of "eye"
+
+    //Examples:
+    // "eye" - should return true
+    // "not a palindrome" - should return false
+    // "A man, a plan, a canal. Panama" - should return true
+    // "_eye" - should return true
+    // "1 eye for of 1 eye." - should return false
+
+
+
